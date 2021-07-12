42909e86a3456ddd1387ef4a979fb8fd281cb8fa
haoyudu <haoyudu@umich.edu>
2021-07-12 09:49:15 -0700

    added combine function

--- Preprocessing/split_csv.py
+++ Preprocessing/split_csv.py
@@ -1,10 +1,14 @@
+# this program should be in the same dir as Tomas' preprocess.py
 import csv
 import os
+import pandas as pd
+import preprocess as pp
 
 input_csv = 'aaa.csv'
 output_dir = 'result'
 output_prefix = 'splitted_aaa'
 
+# you don't have to make a new dir every time you split, putting them in one folder will allow the combine function to access it easily
 os.makedirs(output_dir, exist_ok=True)
 
 max_batch_size = 2
@@ -40,7 +44,22 @@ with open(input_csv, 'r') as csv_in:
         cur_batch_size += 1
     csv_out.close()
 
+# only do the following when you have separated all four variables
+for i in range(50):
+    f_interactors = 'combined_2012/gen1.5_interactors_' + str(i) + '.csv'
+    f_predictors = 'combined_2012/gen1.5_predictors_' + str(i) + '.csv'
+    f_weights = 'combined_2012/gen1.5_weights_' + str(i) + '.csv'
+    f_targets = 'combined_2012/gen1.5_targets_' + str(i) + '.csv'
+    interactors_df = pp.read(f_interactors)
+    predictors_df = pp.read(f_predictors)
+    weights_df = pp.read(f_weights)
+    targets_df = pp.read(f_targets)
+    list_df = [predictors_df, interactors_df, targets_df]
+    comb_df = pp.combine2(weights_df, list_df)
+    out_comb = 'combined_2012_' + str(i)
+    comb_df.to_csv(out_comb)
 
+# this will save your csv's to the scratch folder, not the combined folder
 
 
     
